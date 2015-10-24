f007b625dba8d9dd330a0d41329f3c9f8c101666
lolzcalvin <brotherken1996@gmail.com>
2015-10-25 02:05:47 +0900

    BAM
    some changes:
    naming "connect" to "initialise"
    updates (incomplete) on "initialise"

---
+++ VB(assignment)/VB(assignment)/Initialise.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Initialise
+    Inherits MaterialSkin.Controls.MaterialForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MaterialLabel1 = New MaterialSkin.Controls.MaterialLabel()
+        Me.mdb_btn = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.accdb_btn = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.SuspendLayout()
+        '
+        'MaterialLabel1
+        '
+        Me.MaterialLabel1.AutoSize = True
+        Me.MaterialLabel1.Depth = 0
+        Me.MaterialLabel1.Font = New System.Drawing.Font("Roboto", 11.0!)
+        Me.MaterialLabel1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.MaterialLabel1.Location = New System.Drawing.Point(13, 80)
+        Me.MaterialLabel1.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialLabel1.Name = "MaterialLabel1"
+        Me.MaterialLabel1.Size = New System.Drawing.Size(108, 19)
+        Me.MaterialLabel1.TabIndex = 2
+        Me.MaterialLabel1.Text = "MaterialLabel1"
+        '
+        'mdb_btn
+        '
+        Me.mdb_btn.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.mdb_btn.Depth = 0
+        Me.mdb_btn.Location = New System.Drawing.Point(12, 148)
+        Me.mdb_btn.MouseState = MaterialSkin.MouseState.HOVER
+        Me.mdb_btn.Name = "mdb_btn"
+        Me.mdb_btn.Primary = True
+        Me.mdb_btn.Size = New System.Drawing.Size(193, 35)
+        Me.mdb_btn.TabIndex = 3
+        Me.mdb_btn.Text = "Create MDB Database"
+        Me.mdb_btn.UseVisualStyleBackColor = True
+        '
+        'accdb_btn
+        '
+        Me.accdb_btn.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.accdb_btn.Depth = 0
+        Me.accdb_btn.Location = New System.Drawing.Point(12, 201)
+        Me.accdb_btn.MouseState = MaterialSkin.MouseState.HOVER
+        Me.accdb_btn.Name = "accdb_btn"
+        Me.accdb_btn.Primary = True
+        Me.accdb_btn.Size = New System.Drawing.Size(193, 33)
+        Me.accdb_btn.TabIndex = 4
+        Me.accdb_btn.Text = "Create ACCDB Database"
+        Me.accdb_btn.UseVisualStyleBackColor = True
+        '
+        'Initialise
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(251, 264)
+        Me.Controls.Add(Me.accdb_btn)
+        Me.Controls.Add(Me.mdb_btn)
+        Me.Controls.Add(Me.MaterialLabel1)
+        Me.Name = "Initialise"
+        Me.Text = "Initial Setup"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MaterialLabel1 As MaterialSkin.Controls.MaterialLabel
+    Friend WithEvents mdb_btn As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents accdb_btn As MaterialSkin.Controls.MaterialRaisedButton
+End Class
---
+++ VB(assignment)/VB(assignment)/Initialise.vb
@@ -0,0 +1,95 @@
+﻿Imports MaterialSkin
+Imports System.IO 'for file.exists
+Imports ADOX 'for creating database
+Imports System.Data.OleDb
+
+Public Class Initialise
+    Dim cat As Catalog = New Catalog()
+    Dim dirdb As String = Application.StartupPath + "\database.mdb"
+    Dim dirdb2 As String = Application.StartupPath + "\database.accdb"
+
+    Private Sub new_element()
+        mdb_btn.Dispose()
+        accdb_btn.Dispose()
+
+        MyBase.Height = 350
+        MaterialLabel1.Text = "Please create a new " & vbNewLine & "administrator account."
+
+        Dim label_uname, label_pwd, label_conpwd As New MaterialSkin.Controls.MaterialLabel
+        label_uname.Location = New Point(13, 130)
+        label_uname.Visible = True
+        MaterialLabel1.AutoSize = True
+        label_uname.Text = "test"
+        Me.Controls.Add(label_uname)
+
+
+        Dim field_uname, field_pwd, field_conpwd As New MaterialSkin.Controls.MaterialSingleLineTextField
+
+
+    End Sub
+
+    Partial Class Initialise
+        Inherits MaterialSkin.Controls.MaterialForm
+    End Class
+
+    Private Sub Initialise_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim SkinManager As MaterialSkinManager = MaterialSkinManager.Instance
+        SkinManager.AddFormToManage(Me)
+        SkinManager.Theme = MaterialSkinManager.Themes.LIGHT
+        SkinManager.ColorScheme = New ColorScheme(Primary.DeepPurple400, Primary.DeepPurple600, Primary.DeepPurple700, Accent.DeepOrange700, TextShade.WHITE)
+
+        'btn_appearance() ' :)
+
+        'If File.Exists(dirdb) Or File.Exists(dirdb2) Then
+        '    login.Show()
+        '    Me.Close()
+        'Else
+        '    MessageBox.Show("Please select a suitable access database file extension type.", "Database is not detected")
+        '    MaterialLabel1.Text = "Select from the following:"
+        'End If
+
+        new_element() 'debug purpose for the initial create account new_element. I will remove it by myself.
+
+    End Sub
+
+    Private Sub btn_appearance() 'override buttons' appearances in the form of size and position
+        mdb_btn.Width = 200
+        accdb_btn.Width = 200
+        mdb_btn.Height = 30
+        accdb_btn.Height = 30
+        mdb_btn.Left = (Me.Width / 2) - (mdb_btn.Width / 2)
+        accdb_btn.Left = (Me.Width / 2) - (accdb_btn.Width / 2)
+    End Sub
+
+    Private Sub mdb_btn_Click(sender As Object, e As EventArgs) Handles mdb_btn.Click
+        cat.Create("Provider=Microsoft.Jet.OLEDB.4.0;" &
+                    "Data Source=" & dirdb &
+                    ";Jet OLEDB:Engine Type=5")
+        cat = Nothing
+
+        MessageBox.Show("Database has been successfully created. " +
+                        "This type of database is compatible with Microsoft Access 2002 and above.", "Successful!")
+        new_element()
+
+    End Sub
+
+    Private Sub accdb_btn_Click(sender As Object, e As EventArgs) Handles accdb_btn.Click
+        Try
+            cat.Create("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + dirdb2)
+        Catch
+            Try
+                cat.Create("Provider=Microsoft.ACE.OLEDB.14.0;Data Source=" + dirdb2)
+            Catch
+                cat.Create("Provider=Microsoft.ACE.OLEDB.15.0;Data Source=" + dirdb2)
+            End Try
+        End Try
+
+        cat = Nothing
+
+        MessageBox.Show("Database has been successfully created. " +
+                        "This type of database is compatible with Microsoft Access 2007 and above.", "Successful!")
+        new_element()
+    End Sub
+
+
+End Class
