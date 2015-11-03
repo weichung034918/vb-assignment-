9184e778022a73f7cfc79ef517cbff33dc909342
weichung034918 <weichung034918@gmail.com>
2015-11-03 18:54:27 +0800

    Revert "push some codes, weichung read desc"
    This reverts commit e22142f6ef4e28188c2df8a5b8733cfd70b9004b.

---
+++ VB(assignment)/VB(assignment)/popup.Designer.vb
@@ -0,0 +1,104 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class popup
+    Inherits MaterialSkin.Controls.MaterialForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txt_con_urpwd = New MaterialSkin.Controls.MaterialSingleLineTextField()
+        Me.label_urpwd = New MaterialSkin.Controls.MaterialLabel()
+        Me.btn_del = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.btn_edit = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.SuspendLayout()
+        '
+        'txt_con_urpwd
+        '
+        Me.txt_con_urpwd.Depth = 0
+        Me.txt_con_urpwd.Hint = ""
+        Me.txt_con_urpwd.Location = New System.Drawing.Point(83, 97)
+        Me.txt_con_urpwd.MouseState = MaterialSkin.MouseState.HOVER
+        Me.txt_con_urpwd.Name = "txt_con_urpwd"
+        Me.txt_con_urpwd.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.txt_con_urpwd.SelectedText = ""
+        Me.txt_con_urpwd.SelectionLength = 0
+        Me.txt_con_urpwd.SelectionStart = 0
+        Me.txt_con_urpwd.Size = New System.Drawing.Size(100, 23)
+        Me.txt_con_urpwd.TabIndex = 18
+        Me.txt_con_urpwd.UseSystemPasswordChar = False
+        '
+        'label_urpwd
+        '
+        Me.label_urpwd.AutoSize = True
+        Me.label_urpwd.Depth = 0
+        Me.label_urpwd.Font = New System.Drawing.Font("Roboto", 11.0!)
+        Me.label_urpwd.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.label_urpwd.Location = New System.Drawing.Point(66, 75)
+        Me.label_urpwd.MouseState = MaterialSkin.MouseState.HOVER
+        Me.label_urpwd.Name = "label_urpwd"
+        Me.label_urpwd.Size = New System.Drawing.Size(114, 19)
+        Me.label_urpwd.TabIndex = 16
+        Me.label_urpwd.Text = "Your Password:"
+        '
+        'btn_del
+        '
+        Me.btn_del.Depth = 0
+        Me.btn_del.Location = New System.Drawing.Point(140, 139)
+        Me.btn_del.MouseState = MaterialSkin.MouseState.HOVER
+        Me.btn_del.Name = "btn_del"
+        Me.btn_del.Primary = True
+        Me.btn_del.Size = New System.Drawing.Size(75, 25)
+        Me.btn_del.TabIndex = 21
+        Me.btn_del.Text = "delete"
+        Me.btn_del.UseVisualStyleBackColor = True
+        Me.btn_del.Visible = False
+        '
+        'btn_edit
+        '
+        Me.btn_edit.Depth = 0
+        Me.btn_edit.Location = New System.Drawing.Point(46, 139)
+        Me.btn_edit.MouseState = MaterialSkin.MouseState.HOVER
+        Me.btn_edit.Name = "btn_edit"
+        Me.btn_edit.Primary = True
+        Me.btn_edit.Size = New System.Drawing.Size(75, 25)
+        Me.btn_edit.TabIndex = 20
+        Me.btn_edit.Text = "update"
+        Me.btn_edit.UseVisualStyleBackColor = True
+        Me.btn_edit.Visible = False
+        '
+        'popup
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(240, 208)
+        Me.Controls.Add(Me.btn_del)
+        Me.Controls.Add(Me.btn_edit)
+        Me.Controls.Add(Me.txt_con_urpwd)
+        Me.Controls.Add(Me.label_urpwd)
+        Me.Name = "popup"
+        Me.Text = "Edit/Delete User"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txt_con_urpwd As MaterialSkin.Controls.MaterialSingleLineTextField
+    Friend WithEvents label_urpwd As MaterialSkin.Controls.MaterialLabel
+    Friend WithEvents btn_del As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents btn_edit As MaterialSkin.Controls.MaterialRaisedButton
+End Class
---
+++ VB(assignment)/VB(assignment)/popup.vb
@@ -0,0 +1,33 @@
+﻿Imports MaterialSkin
+Imports System.Text.RegularExpressions
+Imports System.Data.OleDb
+
+Public Class popup
+    Dim dt As New DataTable
+    Partial Class popup
+        Inherits MaterialSkin.Controls.MaterialForm
+    End Class
+
+    Private Sub popup_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim skinmanager As MaterialSkin.MaterialSkinManager = MaterialSkinManager.Instance
+        skinmanager.AddFormToManage(Me)
+        skinmanager.Theme = MaterialSkinManager.Themes.LIGHT
+        skinmanager.ColorScheme = New ColorScheme(Primary.DeepPurple400, Primary.DeepPurple600, Primary.DeepPurple700, Accent.DeepPurple100, TextShade.WHITE)
+        label_urpwd.Left = Me.Width / 2 - label_urpwd.Width / 2
+        txt_con_urpwd.Top = label_urpwd.Top + 25
+        txt_con_urpwd.Left = Me.Width / 2 - txt_con_urpwd.Width / 2
+
+        btn_del.Left = Me.Width / 2 - btn_del.Width / 2
+        btn_edit.Left = btn_del.Left
+        btn_del.Top = txt_con_urpwd.Top + 30
+        btn_edit.Top = btn_del.Top
+        Me.TopMost = True
+        txt_con_urpwd.PasswordChar = "•"
+    End Sub
+
+    Private Sub btn_edit_Click(sender As Object, e As EventArgs) Handles btn_edit.Click
+        If txt_con_urpwd.Text <> dt.Rows(0).Item(1) Then
+            MsgBox("lol")
+        End If
+    End Sub
+End Class
