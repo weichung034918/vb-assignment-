74a92dce9e97961bebab3ae7160844852404df82
weichung034918 <weichung034918@gmail.com>
2015-11-03 19:17:17 +0800

    Revert "Revert "push some codes, weichung read desc""
    This reverts commit 9184e778022a73f7cfc79ef517cbff33dc909342.

--- VB(assignment)/VB(assignment)/My Project/Application.Designer.vb
+++ VB(assignment)/VB(assignment)/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.VB_assignment_.mainpage
+            Me.MainForm = Global.VB_assignment_.Initialise
         End Sub
     End Class
 End Namespace
