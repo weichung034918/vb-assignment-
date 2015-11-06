bdcac6beee07d83135b4ebd7a59c76d3ee183992
lolzcalvin <brotherken1996@gmail.com>
2015-11-06 22:14:46 +0900

    b

--- VB(assignment)/VB(assignment)/My Project/Application.Designer.vb
+++ VB(assignment)/VB(assignment)/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.VB_assignment_.Permission
+            Me.MainForm = Global.VB_assignment_.Initialise
         End Sub
     End Class
 End Namespace
